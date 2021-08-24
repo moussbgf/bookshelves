499fdfbfa998f141a1b34b62d77d0eb29613cd8d
moussbgf <mboughazif@gmail.com>
2021-08-24 15:21:31 +0200

    feat/integration-firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+// import * as firebase from 'firebase';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyAaitWWQuAVJn2TjfMpq_iyKHWh_q4UEMg",
+      authDomain: "bookshelves-43f2c.firebaseapp.com",
+      projectId: "bookshelves-43f2c",
+      storageBucket: "bookshelves-43f2c.appspot.com",
+      messagingSenderId: "549772506869",
+      appId: "1:549772506869:web:4085895005f00c376677bc",
+      measurementId: "G-QBQ41X54YN"
+    };
+    firebase.initializeApp(config);
+  }
 }
